ea4d24e18a9283956df68c30b7337aef2689d2cb
Alexsis Conley Brown <alexsismcb@gmail.com>
2020-10-11 05:59:20 -0500

    graphql and apollo

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,31 @@
+// import mongoose models
+const { User, Thought } = require('../models');
+
+// set up resolver that serves response for query
+const resolvers = {
+    Query: {
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+        // get all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+    }
+};
+
+module.exports = resolvers;
